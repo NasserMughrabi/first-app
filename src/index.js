25d50e4758df8b0f8ab36ce004f30397034501e5
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-06-16 12:58:13 -0600

    completed first react project/app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom/client";
+import './index.css'
 
-function Greeting(){
-
-  return <h1>Hello World!!</h1>;
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Book = () => {
+  return (
+  <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+  );
+}
+
+const Title = () => {
+  return (
+    <h1>I love You to the Moon and Back</h1>
+  );
+}
+
+const Author = () => {
+  return (
+    <h4>John Smilga</h4>
+  );
+}
+
+const Image = () => {
+  return (
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg/330px-Gutenberg_Bible%2C_Lenox_Copy%2C_New_York_Public_Library%2C_2009._Pic_01.jpg" alt="" />
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList/>);
 
